84de225e58b56d4ac8cfba54f359e427f62b8ae7
Alexander Gustafsson Flink <Alexander.GustafssonFlink@gmail.com>
2022-09-13 16:51:38 +0200

    added analytics

--- src/main.js
+++ src/main.js
@@ -3,5 +3,9 @@ import App from './App.vue'
 import quasarUserOptions from '../quasar-user-options'
 import { Quasar } from "quasar"
 import router from "./router/index.js";
+import VueAnalytics from 'vue-analytics';
 
-createApp(App).use(router).use(Quasar, quasarUserOptions).mount('#app')
+createApp(App).use(router).use(VueAnalytics, {
+    id: 'G-W4SH1ZH8QD',
+    router
+}).use(Quasar, quasarUserOptions).mount('#app');
